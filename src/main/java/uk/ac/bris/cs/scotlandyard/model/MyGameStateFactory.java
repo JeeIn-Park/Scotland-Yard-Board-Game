458b34aaf99a282499dc9cb20560cf376caffde7
JeeIn-Park <1700pji@naver.com>
2022-03-30 17:48:33 +0100

    added what i need to implement, pass:80

--- src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/model/MyGameStateFactory.java
@@ -4,22 +4,98 @@ import com.google.common.collect.ImmutableList;
 
 import javax.annotation.Nonnull;
 
+import com.google.common.collect.ImmutableSet;
 import uk.ac.bris.cs.scotlandyard.model.Board.GameState;
 import uk.ac.bris.cs.scotlandyard.model.ScotlandYard.Factory;
+
+import java.util.Optional;
 
 /**
  * cw-model
  * Stage 1: Complete this class
  */
 public final class MyGameStateFactory implements Factory<GameState> {
+	private final class MyGameState implements GameState {
+		/**
+		 * @return the current game setup
+		 */
+		@Nonnull @Override
+		public GameSetup getSetup() {
+			return null;
+		}
+
+		/**
+		 * @return all players in the game
+		 */
+		@Nonnull @Override
+		public ImmutableSet<Piece> getPlayers() {
+			return null;
+		}
+
+		/**
+		 * Computes the next game state given a move from {@link #getAvailableMoves()} has been
+		 * chosen and supplied as the parameter
+		 *
+		 * @param move the move to make
+		 * @return the game state of which the given move has been made
+		 * @throws IllegalArgumentException if the move was not a move from
+		 *                                  {@link #getAvailableMoves()}
+		 */
+		@Nonnull @Override
+		public GameState advance(Move move) {
+			return null;
+		}
+
+		/**
+		 * @param detective the detective
+		 * @return the location of the given detective; empty if the detective is not part of the game
+		 */
+		@Nonnull @Override
+		public Optional<Integer> getDetectiveLocation(Piece.Detective detective) {
+			return Optional.empty();
+		}
+
+		/**
+		 * @param piece the player piece
+		 * @return the ticket board of the given player; empty if the player is not part of the game
+		 */
+		@Nonnull @Override
+		public Optional<Board.TicketBoard> getPlayerTickets(Piece piece) {
+			return Optional.empty();
+		}
+
+		/**
+		 * @return MrX's travel log as a list of {@link LogEntry}s.
+		 */
+		@Nonnull @Override
+		public ImmutableList<LogEntry> getMrXTravelLog() {
+			return null;
+		}
+
+		/**
+		 * @return the winner of this game; empty if the game has no winners yet
+		 * This is mutually exclusive with {@link #getAvailableMoves()}
+		 */
+		@Nonnull @Override
+		public ImmutableSet<Piece> getWinner() {
+			return null;
+		}
+
+		/**
+		 * @return the current available moves of the game.
+		 * This is mutually exclusive with {@link #getWinner()}
+		 */
+		@Nonnull @Override
+		public ImmutableSet<Move> getAvailableMoves() {
+			return null;
+		}
+	}
+
 
 	@Nonnull @Override public GameState build(
 			GameSetup setup,
 			Player mrX,
-			ImmutableList<Player> detectives) {
-		// TODO
-		throw new RuntimeException("Implement me!");
+			ImmutableList<Player> detectives) {return null;}
 
-	}
 
 }
